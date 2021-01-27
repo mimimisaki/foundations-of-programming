86489a237a15d7a6e84434bbfcdbb65a2032d307
mimimisaki <mimimisaki2@gmail.com>
2021-01-27 23:56:22 +0900

    [Finish] chapter8.5

---
+++ src/chapter8/8-2.ml
@@ -0,0 +1,27 @@
+type okozukai_t = {
+    name : string;
+    price : int;
+    place : string;
+    date : string;
+}
+
+let item1 = { 
+  name = "プログラミングの基礎"; 
+  price = 2415; 
+  place = "大学生協書籍部"; 
+  date = "2007/3/20"; 
+} 
+ 
+let item2 = { 
+  name = "午後の紅茶"; 
+  price = 150; 
+  place = "ローソン"; 
+  date = "2007/3/22"; 
+} 
+ 
+let item3 = { 
+  name = "置き時計"; 
+  price = 1200; 
+  place = "イトーヨーカドー"; 
+  date = "2007/3/28"; 
+}
---
+++ src/chapter8/8-3.ml
@@ -0,0 +1,35 @@
+type person_t = {
+    name : string;
+    height : float;
+    weight : float;
+    month : int;
+    day : int;
+    blood : string;
+}
+
+let person1 = {
+    name = "浅井"; 
+   height = 1.72; 
+   weight = 58.5; 
+   month = 9; 
+   day = 17; 
+   blood = "A" 
+} 
+ 
+let person2 = { 
+  name = "宮原"; 
+  height = 1.63; 
+  weight = 55.0; 
+  month = 6; 
+  day = 30; 
+  blood = "B" 
+} 
+ 
+let person3 = { 
+  name = "中村"; 
+  height = 1.68; 
+  weight = 63.0; 
+  month = 6; 
+  day = 6; 
+  blood = "O" 
+}
